976716e91a265bf70f06006ed0c3ee02e884dc83
pcolliander <pontus.colliander@gmail.com>
2017-11-22 19:32:13 +0100

    put elm-stuff in gitignore; made value disappear when creating a todo

--- main.elm
+++ main.elm
@@ -8,38 +8,35 @@ main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
 -- MODEL
-type alias Model = 
-  { todos : List Todo,
-    inputValue : String }
+type alias Model = { 
+  todos : List Todo,
+  inputValue : String 
+}
 
 type alias Todo = { id : Int, title : String, isCompleted : Bool}
 
 model : Model
-model = 
-  { todos = [],
-    inputValue = ""}
+model = { todos = [], inputValue = ""}
 
-(=>) = (,)
 
 -- UPDATE
-type Msg 
-  = Create 
-  | Change String 
-  | Delete Int
-  | ToggleComplete Int
+type Msg = Create 
+           | Change String 
+           | Delete Int
+           | ToggleComplete Int
 
 update msg model =
   case msg of
     Create -> 
       { model | 
-          todos = Todo (List.length model.todos + 1) model.inputValue False :: model.todos }
+        todos = Todo (List.length model.todos + 1) model.inputValue False :: model.todos,
+        inputValue = ""}
 
     Change newContent -> 
       { model | inputValue = newContent } 
 
     Delete id ->
-      { model | 
-          todos = filter (\todo -> todo.id /= id) model.todos }
+      { model | todos = filter (\todo -> todo.id /= id) model.todos }
 
     ToggleComplete id -> 
       let 
@@ -49,41 +46,21 @@ update msg model =
       in 
         { model | todos = List.map updateEntry model.todos }
 
-      -- ALSO WORKING (My Own solution)
-      -- let 
-      --   todo = 
-      --     model.todos 
-      --     |> filter (\todo -> todo.id == id)
-      --     |> List.map (\todo -> {todo | isCompleted = not todo.isCompleted } )
-      --     |> head
-      --
-      -- in
-      --   case todo of
-      --     Just value ->
-      --       { model |
-      --         todos = 
-      --           value :: (filter (\t -> t.id /= id) model.todos)}
-      --     Nothing ->
-      --       model
-
 -- VIEW
+(=>) = (,)
+
 view model =
   div [style 
     [ "display" => "flex",
       "flex-direction" => "column",
       "width" => "250px",
       "margin" => "50px auto" ]]
-
-    [ input [placeholder "Write something", onInput Change ] [], 
-
+    [ 
+      input [placeholder "Write something", onInput Change, value model.inputValue ] [], 
       model.todos
         |> sortBy .id
-        |> List.map (\todo -> div [ style [ "display" => "flex", "justify-content" => "space-between" ]] [ li [ onClick (ToggleComplete todo.id), style [ "text-decoration" => (todo.isCompleted ? "line-through" <| "")]] [ text todo.title ], span [ style [ "cursor" => "pointer"], onClick (Delete todo.id)] [ text "X" ]] )
+        |> List.map (\todo -> div [ style [ "display" => "flex", "justify-content" => "space-between" ]] [ li [ onClick (ToggleComplete todo.id), style [ "user-select" => "none", "text-decoration" => (todo.isCompleted ? "line-through" <| "")]] [ text todo.title ], span [ style [ "cursor" => "pointer"], onClick (Delete todo.id)] [ text "X" ]] )
         |> ul []
 
-     , button [ onClick Create ] [ text "Create" ] 
-    ]
-
-
-
+     , button [ onClick Create ] [ text "Create" ]]
 
